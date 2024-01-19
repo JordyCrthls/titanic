970a92a199a0c8b655dbbe85dc7b44554ea8a900
Jordy <r0740464@student.vives.be>
2024-01-19 20:15:29 +0100

    Refactor train_model function and modify code cells in analitycs notebook
    The 'train_model' function in training.py is refactored to improve the train-test split. Labels were isolated in a separate variable from the dataframe before the splitting process for clarity. The splitting ratios are also adjusted and a random state is set for reproducibility. Code cells in analitycs.ipynb were modified for better data inspection and presentation. Commentary was added for clarity and the data information checkout processes were extended.

--- training.py
+++ training.py
@@ -5,9 +5,9 @@ from sklearn.model_selection import train_test_split
 
 
 def train_model(data, num_epochs=50):
+    labels = data['Survived']
     dataframe = data.drop(['Survived'], axis=1)
-
-    train_data, unused_train, test_data, unused_test = train_test_split(dataframe, data['Survived'], test_size=0.9)
+    train_data, test_data, train_labels, test_labels = train_test_split(dataframe, labels, test_size=0.2, random_state=1)
     input = layers.Input(shape=(4, 1, 1))
     layer_build = layers.Flatten()(input)
     layer_build = layers.Dense(57, activation='relu')(layer_build)
@@ -15,7 +15,7 @@ def train_model(data, num_epochs=50):
     output = layers.Dense(1, activation='sigmoid')(layer_build)
     model = keras.Model(inputs=input, outputs=output)
     model.compile(optimizer='adam', loss='binary_crossentropy')
-    model.fit(train_data, test_data, epochs=num_epochs)
+    model.fit(train_data, train_labels, epochs=num_epochs)
     model.summary()
 
     return model
